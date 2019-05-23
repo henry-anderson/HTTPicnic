bd791c3441449a0312ed58d0af5991c87c979ea6
Henry Anderson <henryanderson1@hotmail.com>
2019-05-23 15:25:37 -0500

    Programatically login to GitHub and follow a user avoiding rate limits

--- examples/GitHub/GitHubAccount.java
+++ examples/GitHub/GitHubAccount.java
@@ -1,4 +1,4 @@
-package anderson.henry.httppicnic.examples.githublogin;
+package anderson.henry.httppicnic.examples.github;
 
 import anderson.henry.httpicnic.PicnicClient;
 import anderson.henry.httpicnic.http.HttpHeader;
@@ -6,7 +6,8 @@ import anderson.henry.httpicnic.http.HttpResponse;
 
 /**
  * This class represents an account on https://github.com
- * It allows you to programmatically sign into GitHub
+ * It allows you to programmatically sign into GitHub and follow or unfollow a user
+ * I didn't use the GitHub API for this class to get around rate limits
  * This class uses HTTPicnic to handle HTTP requests
  * @author Henry Anderson
  */
@@ -28,7 +29,7 @@ public class GitHubAccount {
 	
 	/**
 	 * Logs into GitHub.com
-	 * Note: Before sending a POST request to login you must get an "authenticity token" from a form on the login page
+	 * Note: Before sending the POST request to login you must get an "authenticity token" from a form on the login page
 	 * @return a boolean representing whether the login was successful
 	 */ 
 	public boolean login() {
@@ -43,5 +44,31 @@ public class GitHubAccount {
 		client.addParameter("webauthn-support", "supported");
 		response = this.client.sendPOST("https://github.com/session");
 		return client.getCookie("logged_in").getValue().equalsIgnoreCase("yes");
+	}
+	
+	/**
+	 * Follows a user on GitHub
+	 * Note: Before sending the POST request to follow a user you must get an "authenticity token" from a form on their page
+	 * @param user The name of the user to follow
+	 */
+	public void follow(String user) {
+		HttpResponse response = client.sendGET("https://github.com/" + user);
+		String token = response.getContent().split("name=\"authenticity_token\" value=\"")[4].split("\" />")[0];
+		client.addParameter("utf-8", "&#x2713;");
+		client.addParameter("authenticity_token", token);
+		client.sendPOST("https://github.com/users/follow?target=" + user);
+	}
+	
+	/**
+	 * Unfollows a user on GitHub
+	 * Note: Before sending the POST request to unfollow a user you must get an "authenticity token" from a form on their page
+	 * @param user The name of the user to unfollow
+	 */
+	public void unfollow(String user) {
+		HttpResponse response = client.sendGET("https://github.com/" + user);
+		String token = response.getContent().split("name=\"authenticity_token\" value=\"")[5].split("\" />")[0];
+		client.addParameter("utf-8", "&#x2713;");
+		client.addParameter("authenticity_token", token);
+		client.sendPOST("https://github.com/users/unfollow?target=" + user);
 	}
 }
---
+++ examples/GitHub/Main.java
@@ -0,0 +1,18 @@
+package anderson.henry.httppicnic.examples.github;
+
+public class Main {
+	
+	/**
+	 * Logs into http://github.com and follows the user "henry-anderson"
+	 */
+	public static void main(String[] args) {
+		GitHubAccount account = new GitHubAccount("GitHubUsername", "password123");
+		if(account.login()) {
+			System.out.println("Logged in!");
+			account.follow("henry-anderson");
+		}
+		else {
+			System.out.println("Could not login!");
+		}
+	}
+}
